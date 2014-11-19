9fbfaac74b3213601ce48c73cd49a02344ba580b
ngoyal <ngoyal@verisign.com>
2014-11-19 11:57:13 -0500

    Update to require sispy instead of sis

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup (
     version=sisdb.VERSION,
     description='SIS ORM like library',
     packages=['sisdb'],
-    install_requires=['sis >= 0.3.0']
+    install_requires=['sispy >= 0.3.0']
 )
