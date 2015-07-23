f7c01f27360f05f853a266014c9016df494fd0f1
Neel Goyal <ngoyal@gmail.com>
2015-07-23 10:19:22 -0400

    Release for public

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup (
     version=sisdb.VERSION,
     description='SIS ORM like library',
     packages=['sisdb'],
+    keywords=['sis', 'sis-cmdb', 'orm'],
     install_requires=['sispy >= 0.3.0']
 )
